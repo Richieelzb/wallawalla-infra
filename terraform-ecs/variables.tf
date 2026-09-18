5723e5e1873000d0fcddae92eca1511a09d7b215
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-09-18 12:34:31 +0200

    add

--- terraform-ecs/variables.tf
+++ terraform-ecs/variables.tf
@@ -94,20 +94,6 @@ variable "db_identifier" {
   type = string
 }
 
-variable "database_name" {
-  type = string
-}
-
-variable "master_username" {
-  type      = string
-  sensitive = true
-}
-
-variable "master_password" {
-  type      = string
-  sensitive = true
-}
-
 variable "instance_class" {
   type    = string
 }
